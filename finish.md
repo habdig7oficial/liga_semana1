bf00a536575a098ba31d179b4c316513e0217164
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-04-04 22:00:13 -0300

    ok

---
+++ finish.md
@@ -0,0 +1,12 @@
+## Matrizes: 
+
+1 - OK
+2 -
+3 - 
+
+
+## Vetores
+
+1 - OK 
+2
+3 - OK
---
+++ vetores/ex2.cr
@@ -0,0 +1,8 @@
+require "./lib.cr"
+
+include VectorLib
+
+
+a : VectorInt32 = {3, 4}
+b : VectorInt32 = {4, -3}
+
