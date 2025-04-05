d7aa6178d8fc93fd50652705d0e155eb6929e091
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-04-04 21:55:40 -0300

    sum vecs

--- vetores/ex3.cr
+++ vetores/ex3.cr
@@ -6,12 +6,5 @@ include VectorLib
 a : VectorInt32 = {3, -2}
 b : VectorInt32 = {-1, 4}
 
-op : Array(VectorInt32) = [a,b]
 
-res  = [0, 0]
-op.each do | i, j |
-  res[0] += i 
-  res[1] += j
-end
-
-puts res
+puts sum_vec a, b
--- vetores/lib.cr
+++ vetores/lib.cr
@@ -10,4 +10,14 @@ module VectorLib
     def vec_module(vec : VectorInt32) : Float64
         return sqrt vec[0] ** 2 + vec[1] ** 2
     end
+
+    def sum_vec(*vecs : VectorInt32) 
+        res : Array(Int32) = [0, 0]
+        vecs.each do | i, j |
+            res[0] += i 
+            res[1] += j
+        end
+        t_res : VectorInt32 = { res[0], res[1] }
+        return t_res
+    end
 end
